2ec3795930bd14ea4c209f2bd877398face6528b
Rofty <57068821+therofty911@users.noreply.github.com>
2021-12-07 13:50:01 +0700

    a

---
+++ components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        // <div className="flex-container">
+        //     <div className="flex-items">
+        //         <img className src="https://cdn.discordapp.com/attachments/891579314401869864/917458177207980032/mh-300.png" alt="Movie Hex" width="50" />
+        //     </div>
+        // </div>
+        <div className="warna">
+            <img className="logo-header" src="https://media.discordapp.net/attachments/891579314401869864/917450151151407154/mh.png?width=1440&height=223" alt="Movie Hex" width="50" />
+        </div>
+    )
+}
+
+export default Header
