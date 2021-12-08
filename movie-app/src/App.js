447f9ce87f1ab8af4c244e2325fae3bf75cafef4
Rofty <57068821+therofty911@users.noreply.github.com>
2021-12-08 15:55:11 +0700

    nice

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,17 +1,26 @@
-import Header from './components/Header/Header';
-import SimpleBottomNavigation from './components/MainNav';
-import './App.css';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './App.scss';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import Routes from './config/Routes';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div className="app">
-        ffeff
-      </div>
-      <SimpleBottomNavigation/>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Routes/>
+                    <Footer/>
+                </>
+            )}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
